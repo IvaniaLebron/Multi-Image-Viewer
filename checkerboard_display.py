3591dba5fbf70768f18b6e16d35d2515ab01d7a4
temesgen911 <72382946+temesgen911@users.noreply.github.com>
2023-12-08 13:32:14 +0100

    Update checkerboard_display.py
    Updates: Selecting number of squares and saving the image

--- checkerboard_display.py
+++ checkerboard_display.py
@@ -1,50 +1,58 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
-from PyQt5.QtGui import QPixmap, QImage, QPainter, QBrush, QColor
-from PyQt5.QtCore import Qt, QRect
-
-class CheckerboardDisplayApp(QWidget):
-    def __init__(self, image_path1, image_path2):
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QMessageBox
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from Select_Image import SelectImagesApp
+class ImageViewerApp(QWidget):
+    def __init__(self):
         super().__init__()
 
-        self.image1 = QPixmap(image_path1)
-        self.image2 = QPixmap(image_path2)
-
         # Set window properties
-        self.setWindowTitle('Checkerboard Image Display')
-        self.setGeometry(100, 100, 600, 600)
+        self.setWindowTitle('Image Viewer and Manipulator')
+        self.setGeometry(100, 100, 600, 400)
 
-        # Create a label to display the checkerboard image
-        self.label = QLabel(self)
-        self.display_checkerboard()
+        # Create widgets
+        label = QLabel('Choose an Operation:', self)
+        self.style_label(label)
+
+        btn_checkerboard = QPushButton('View Image', self)
+        btn_checkerboard.clicked.connect(lambda: self.show_option_selected('View Image'))
+        self.style_button(btn_checkerboard)
+
+        btn_image_view = QPushButton('CheckerBoard Effect', self)
+        btn_image_view.clicked.connect(lambda: self.show_option_selected('CheckerBoard Effect'))
+        self.style_button(btn_image_view)
+
+        btn_grid = QPushButton('Grid Effect', self)
+        btn_grid.clicked.connect(lambda: self.show_option_selected('Grid Effect'))
+        self.style_button(btn_grid)
 
         # Set up the layout
         layout = QVBoxLayout(self)
-        layout.addWidget(self.label)
+        layout.addWidget(label, alignment=Qt.AlignCenter)
+        layout.addWidget(btn_checkerboard)
+        layout.addWidget(btn_image_view)
+        layout.addWidget(btn_grid)
+
         self.setLayout(layout)
 
-    def display_checkerboard(self):
-        # Create a new QPixmap to hold the checkerboard pattern
-        checkerboard = QPixmap(600, 600)
-        checkerboard.fill(Qt.white)
-        painter = QPainter(checkerboard)
+    def style_label(self, label):
+        label.setAlignment(Qt.AlignCenter)
+        label.setStyleSheet("font-size: 20px; margin-bottom: 20px; color: #333;")
 
-        # Calculate the size of each checkerboard square
-        square_size = min(self.image1.width(), self.image1.height(), self.image2.width(), self.image2.height()) // 8
+    def style_button(self, button):
+        button.setStyleSheet("font-size: 16px; padding: 15px; margin: 10px; background-color: #3498db; color: white; border: 2px solid #2980b9; border-radius: 8px;")
 
-        for row in range(8):
-            for col in range(8):
-                rect = QRect(col * square_size, row * square_size, square_size, square_size)
-                if (row + col) % 2 == 0:
-                    painter.drawPixmap(rect, self.image1, rect)
-                else:
-                    painter.drawPixmap(rect, self.image2, rect)
-
-        painter.end()
-        self.label.setPixmap(checkerboard)
+    def show_option_selected(self, option):
+        if option == 'CheckerBoard Effect':
+            #Open the SelectImagesApp window
+            self.image_selector = SelectImagesApp('CheckerBoard Effect', 2)
+            self.image_selector.show()
+        else:
+            QMessageBox.information(self, 'Option Selected', f'You selected: {option}', QMessageBox.Ok)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = CheckerboardDisplayApp('path_to_image1.jpg', 'path_to_image2.jpg')  # Replace with actual paths
+    window = ImageViewerApp()
     window.show()
     sys.exit(app.exec_())
