4d9cbac8f8860d8a2b75f467bd0b61263892e44c
Ivania <ivanialebron4.180@gmail,com>
2023-12-09 22:01:11 +0100

    partial zoom in to all images of grid

--- grid_display.py
+++ grid_display.py
@@ -1,10 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QGridLayout
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QGridLayout, QSizePolicy, QLayout
+from PyQt5.QtGui import QPixmap, QMouseEvent, QImage, QPainter
+from PyQt5.QtCore import Qt, QEvent
+from PIL import Image
+import os
 
 class ImageGridApp(QWidget):
-    #This is our constructor, recieves the paths of seleccted images, and the selected grid layout
     def __init__(self, image_paths, rows, cols):
         super().__init__()
 
@@ -23,26 +24,41 @@ class ImageGridApp(QWidget):
         grid_layout = QGridLayout()
         self.setLayout(grid_layout)
 
-        # Calculate number of cells in the grid
-        num_cells = self.rows * self.cols
-
-        for i in range(num_cells):
-            row = i // self.cols
-            col = i % self.cols
-
-            label = QLabel(self)
-            label.setPixmap(self.images[i % len(self.images)])
-            grid_layout.addWidget(label, row, col)
+        self.labels = []  # To keep references to labels for later updates
+        for i in range(self.rows):
+            for j in range(self.cols):
+                label = ClickableLabel(self, i, j)
+                label.setPixmap(self.images[(i * self.cols + j) % len(self.images)])
+                grid_layout.addWidget(label, i, j)
+                self.labels.append(label)
 
         self.setWindowTitle(f"Image Grid ({self.rows}x{self.cols})")
         self.show()
 
+class ClickableLabel(QLabel):
+    def __init__(self, parent, row, col):
+        super().__init__(parent)
+        self.row = row
+        self.col = col
+
+    def mousePressEvent(self, event):
+        # Handle mouse click event
+        if event.button() == Qt.LeftButton:
+            # Zoom in on the clicked image and all others in the same position
+            for label in self.parent().labels:
+                if label.row == self.row and label.col == self.col:
+                    label.setScaledContents(True)
+                    label.setFixedSize(200, 200)  # Set the desired size for zoomed images
+                else:
+                    label.setScaledContents(False)
+                    label.setFixedSize(100, 100)  # Reset the size for other images
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    image_paths = ['path_to_image1.jpg', 'path_to_image2.jpg', 'path_to_image3.jpg', 'path_to_image4.jpg']  # Add more paths as needed
-    rows = 2  
-    cols = 2  
+    image_paths = ['path_to_image1.jpg', 'path_to_image2.jpg', 'path_to_image3.jpg', 'path_to_image4.jpg']
+    rows = 2
+    cols = 2
 
     window = ImageGridApp(image_paths, rows, cols)
     sys.exit(app.exec_())
