f31ffaf9ed0a430a3df0d93a5a5bb9318f268416
Ivania <ivanialebron4.180@gmail,com>
2023-12-12 00:45:40 +0100

    Simultaneous zoom (with scale error)

--- grid_display.py
+++ grid_display.py
@@ -1,22 +1,35 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QWidget, QVBoxLayout, QScrollArea, QGridLayout
 from PyQt5.QtGui import QPixmap, QImage, QPainter
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 import cv2
 
 class ZoomableGraphicsView(QGraphicsView):
+    zoomChanged = pyqtSignal(float)
+
     def __init__(self, scene, parent=None):
         super(ZoomableGraphicsView, self).__init__(scene, parent)
         self.setRenderHint(QPainter.Antialiasing, True)
         self.setRenderHint(QPainter.SmoothPixmapTransform, True)
         self.setRenderHint(QPainter.HighQualityAntialiasing, True)
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
+        self.global_zoom_factor = 1.0  # Initialize the global zoom factor
 
     def wheelEvent(self, event):
         factor = 1.2
         if event.angleDelta().y() < 0:
             factor = 1.0 / factor
 
-        self.scale(factor, factor)
+        # Get the position of the mouse cursor in the scene
+        mouse_scene_pos = self.mapToScene(event.pos())
+
+        # Apply the zoom centered around the mouse cursor
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setTransform(self.transform().translate(mouse_scene_pos.x(), mouse_scene_pos.y()).scale(factor, factor).translate(-mouse_scene_pos.x(), -mouse_scene_pos.y()))
+
+        self.global_zoom_factor = self.transform().m11()
+        self.zoomChanged.emit(self.global_zoom_factor)
 
 class GridDisplayApp(QWidget):
     def __init__(self, image_paths, rows, cols):
@@ -33,6 +46,7 @@ class GridDisplayApp(QWidget):
 
     def initUI(self):
         grid_layout = QGridLayout(self)
+        self.views = []
 
         for row in range(self.rows):
             for col in range(self.cols):
@@ -41,16 +55,17 @@ class GridDisplayApp(QWidget):
                     image_path = self.image_paths[index]
                     pixmap = self.load_image(image_path)
 
-                    scene = QGraphicsScene(self)  # Create a new scene for each image
+                    scene = QGraphicsScene(self)
                     item = QGraphicsPixmapItem(pixmap)
                     scene.addItem(item)
 
                     view = ZoomableGraphicsView(scene, self)
-                    view.setSceneRect(scene.sceneRect())
                     view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
                     view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
                     grid_layout.addWidget(view, row, col)
+
+                    view.zoomChanged.connect(self.handleZoomChange)
+                    self.views.append(view)
 
         widget = QWidget(self)
         widget.setLayout(grid_layout)
@@ -73,10 +88,16 @@ class GridDisplayApp(QWidget):
         pixmap = QPixmap.fromImage(qimg)
         return pixmap
 
+    def handleZoomChange(self, zoom_factor):
+        for view in self.views:
+            if view.global_zoom_factor != zoom_factor:
+                view.setTransform(view.transform().scale(zoom_factor, zoom_factor))
+                view.global_zoom_factor = zoom_factor
+
 if __name__ == '__main__':
     try:
         app = QApplication(sys.argv)
-        image_paths = ['path_to_image1.jpg', 'path_to_image2.jpg', 'path_to_image3.jpg', 'path_to_image4.jpg']  # Add more paths as needed
+        image_paths = ['path_to_image1.jpg', 'path_to_image2.jpg', 'path_to_image3.jpg', 'path_to_image4.jpg']
         rows = 2
         cols = 2
 
@@ -84,4 +105,4 @@ if __name__ == '__main__':
         window.show()
         sys.exit(app.exec_())
     except Exception as e:
-        print(f"Error: {str(e)}")
+        print(f"Error in main block: {str(e)}")
