b545507bba07cf085ae17264435ce19a63909cc3
anayagio25 <anayagio25@gmail.com>
2023-12-20 21:17:01 +0100

    Updated UI

--- ImageViewer.py
+++ ImageViewer.py
@@ -1,51 +1,32 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel,  QPushButton, QListWidget, QWidget, QMessageBox,QHBoxLayout, QVBoxLayout, QSplitter, QScrollArea
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QFileDialog
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtCore import QPropertyAnimation, QRect, Qt
 
-class ImageViewer(QWidget):
-  def __init__(self):
+class ImageViewApp(QWidget):
+    def __init__(self):
         super().__init__()
 
-        # Set window properties
-        self.setWindowTitle(f'Image Viewer')
-        self.setGeometry(100, 100, 600, 300)
-        self.style_window(self)
+        self.setWindowTitle('Image Viewer')
+        self.setGeometry(100, 100, 800, 600)
 
-        # Create widgets
-        show_image = QLabel(self)
-        show_image.setAlignment(Qt.AlignCenter)
-        self.style_image(show_image)
+        self.current_label = QLabel(self)
+        self.current_label.setGeometry(0, 0, 800, 600)
+        self.current_label.setScaledContents(True)
 
-        btn_previous_image = QPushButton('<', self)
-        self.style_button(btn_previous_image)
-        btn_previous_image.setFixedSize(75, 200)
+        self.next_label = QLabel(self)
+        self.next_label.setGeometry(800, 0, 800, 600)  # Start off-screen
+        self.next_label.setScaledContents(True)
 
-        btn_next_image = QPushButton('>', self)
-        self.style_button(btn_next_image)
-        btn_next_image.setFixedSize(75,200)
+        self.next_button = QPushButton(">", self)
+        self.next_button.setGeometry(760, 280, 40, 40)
 
-        layout = QHBoxLayout(self)
-        layout.addWidget(btn_previous_image)
-        layout.addWidget(show_image)
-        layout.addWidget(btn_next_image)
+        self.prev_button = QPushButton("<", self)
+        self.prev_button.setGeometry(0, 280, 40, 40)
 
-        
-
-  def style_window(self, window):
-       window.setStyleSheet("background-color: #343536;")
-
-  def style_image(self, label):
-        label.setStyleSheet("QLabel { background-color: grey; border-radius: 10px; }")
-
-  def style_button(self, button):
-        button.setStyleSheet("font-size: 25px; font-weight: bold; padding: 15px; margin: 10px; background-color:grey ; color: black; border: 2px solid grey; border-radius: 8px;")
-
-  
-       
-  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = ImageViewer()
+    window = ImageViewApp()
     window.show()
     sys.exit(app.exec_())
